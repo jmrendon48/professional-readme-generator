cb6db3e8743ec2bbaa547bac2fb62777f9fe5b73
AlexDeLarge <jmrendon48@gmail.com>
2021-02-13 23:58:14 -0800

    added questions for user input

--- index.js
+++ index.js
@@ -1,7 +1,140 @@
 // TODO: Include packages needed for this application
+const fs = require('fs');
+const inquirer = require('inquirer');
 
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+    {
+        type: 'input',
+        name: 'title',
+        message: 'What is the title of your project?',
+        validate: titleInput => {
+            if (titleInput) {
+                return true;
+            } else {
+                console.log('Please enter title of your project.');
+                return false;
+            }
+        }
+    },
+    {
+        type: 'input',
+        name: 'description',
+        message: 'Enter description of your project.',
+        validate: descriptionInput => {
+            if (descriptionInput) {
+                return true;
+            } else {
+                console.log('Please enter description of your project.');
+                return false;
+            }
+        }
+    },
+    {
+        type: 'confirm',
+        name: 'confirmInstallation',
+        message: 'Would you like to enter instructions on how to install your application?',
+        default: true
+    },
+    {
+        type: 'input',
+        name: 'installation instructions',
+        message: 'Enter instructions on how to install your application.',
+        when: ({ confirmInstallation }) => {
+            if (confirmAbout) {
+              return true;
+            } else {
+              return false;
+            }
+        }
+    },
+    {
+        type: 'input',
+        name: 'usage',
+        message: 'Enter instructions and examples for use.',
+        validate: usageInput => {
+            if (usageInput) {
+                return true;
+            } else {
+                console.log('Please enter instructions on how to use the application.');
+                return false;
+            }
+        }
+    },
+    {
+        type: 'input',
+        name: 'contribution guidelines',
+        message: 'Enter guidelines for how you would like others to contribute to the application.',
+        validate: contributionInput => {
+            if (contributionInput) {
+                return true;
+            } else {
+                console.log('Please enter guidelines for future contribution.');
+                return false;
+            }
+        }
+    },
+    {
+        type: 'confirm',
+        name: 'confirmTests',
+        message: 'Would you like to provide tests for your application?'
+    }
+    {
+        type: 'input',
+        name: 'tests',
+        message: 'Enter tests written for your application, providing examples on how to run them.',
+        when: ({ confirmTests }) => {
+            if (confirmAbout) {
+              return true;
+            } else {
+              return false;
+            }
+        }
+    },
+    {
+        type: 'list',
+        name: 'license',
+        message: 'Choose a license.',
+        choices: [
+            'Apache License 2.0',
+            'Boost Software License 1.0',
+            'GNU AGPL v3',
+            'GNU GPL v3',
+            'GNU LGPL v3',
+            'GNU FDLv 1.3',
+            'ISC',
+            'Mozilla Public License 2.0',
+            'MIT License',
+            'N/a'
+        ]
+    },
+    {
+        type: 'input',
+        name: 'username',
+        message: 'Enter GitHub username, which a link to the respective profile will provided for.',
+        validate: usernameInput => {
+            if (usernameInput) {
+                return true;
+            } else {
+                console.log('Please enter GitHub username.');
+                return false;
+            }
+        }
+    },
+    {
+        type: 'input',
+        name: 'email',
+        message: 'Enter email address.',
+        validate: emailInput => {
+            if (emailInput) {
+                return true;
+            } else {
+                console.log('Please enter email address.');
+                return false;
+            }
+        }
+    },
+];
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
