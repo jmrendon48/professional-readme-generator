4f20770eae7029d81ad8b9054787d5db51558175
AlexDeLarge <jmrendon48@gmail.com>
2021-02-14 00:54:25 -0800

    added fs.writeFile

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 // TODO: Include packages needed for this application
 const fs = require('fs');
 const inquirer = require('inquirer');
+// create page with sections
+const generateMarkdown = require('./utils/generateMarkdown');
 
 // TODO: Create an array of questions for user input
 const questions = [
@@ -78,7 +80,7 @@ const questions = [
         type: 'confirm',
         name: 'confirmTests',
         message: 'Would you like to provide tests for your application?'
-    }
+    },
     {
         type: 'input',
         name: 'tests',
@@ -136,11 +138,30 @@ const questions = [
     },
 ];
 
-// TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+// TODO: Create a functon to write README file
+const writeToFile = (fileName, data) => {
+    fs.writeFile('./index.html', data, err => {
+        // if there's an error, reject the Promise and send the error to the Promise's `.catch()` method
+        if (err) {
+            reject(err);
+            // return out of the function here to make sure the Promise doesn't accidentally execute the resolve() function as well
+            return;
+        }
+        // if everything went well, resolve the Promise and send the successful data to the `.then()` method
+        resolve({
+            ok: true,
+            message: 'File created!'
+        });
+    })
+};
 
 // TODO: Create a function to initialize app
-function init() {}
+function init() {
+    return inquirer.prompt(questions)
+};
 
 // Function call to initialize app
-init();
+init()
+    .then(data => {
+        return generateMarkdown(data);
+    });
